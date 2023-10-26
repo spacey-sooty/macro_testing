0c3bfe8cfb7ddf7e0d2201b04b0d71ed47f107ff
Isaac Turner <spacey_sooty@outlook.com>
2023-10-26 23:33:40 +0800

    map vector similar to vec macro for vectors but for hashmaps

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,24 @@
-use proc_macrobrrrr::{make_answer, AnswerFn, HelperAttr, show_streams};
-
 #[macro_export]
-macro_rules! worse_vec {
-    ( $( $x:expr ),* ) => {
+macro_rules! map {
+    ( $( $x:expr, $y:expr ),* ) => {
         {
-            let mut temp_vec = Vec::new();
+            let mut temp_map = std::collections::HashMap::new();
             $(
-                temp_vec.push($x);
+                let mut count = 0;
+                for i in $x {
+                    temp_map.insert(i, $y[count]);
+                    count += 1;
+                }
             )*
-            temp_vec
+            temp_map
         }
     };
 }
 
-#[derive(HelperAttr)]
-struct Struct {
-    #[helper] lol: (),
-}
-
-#[derive(AnswerFn)]
-struct Hello;
-
-make_answer!();
-
 fn main() {
-    let hello = Hello;
-    let vec = worse_vec!["hello", "joe"];
-    println!("{:#?}", vec);
-    println!("{}", hello.answer());
-    println!("{}", answer());
-
-    // Example: Basic function
-    #[show_streams]
-    fn invoke1() {}
-    // out: attr: ""
-    // out: item: "fn invoke1() {}"
-
-    // Example: Attribute with input
-    #[show_streams(bar)]
-    fn invoke2() {}
-    // out: attr: "bar"
-    // out: item: "fn invoke2() {}"
-
-    // Example: Multiple tokens in the input
-    #[show_streams(multiple => tokens)]
-    fn invoke3() {}
-    // out: attr: "multiple => tokens"
-    // out: item: "fn invoke3() {}"
-
-    // Example:
-    #[show_streams { delimiters }]
-    fn invoke4() {}
-    // out: attr: "delimiters"
-    // out: item: "fn invoke4() {}"
+    let map = map!([1, 2, 3],["hello", "joe", "mum"]);
+    for (key, value) in map {
+        println!("\n{:#?}: {:#?}", key, value);
+    }
 }
 
