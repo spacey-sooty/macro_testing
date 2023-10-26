969f2b371f261433364b85f3c3f03435c404d296
Isaac Turner <spacey_sooty@outlook.com>
2023-10-26 21:10:29 +0800

    initial stuff rust book thing doesnt work

---
+++ proc_macro/src/lib.rs
@@ -0,0 +1,34 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+
+#[proc_macro]
+pub fn make_answer(_item: TokenStream) -> TokenStream {
+    "fn answer() -> u32 { 42 }".parse().unwrap()
+}
+
+#[proc_macro_derive(AnswerFn)]
+pub fn derive_answer_fn(item: TokenStream) -> TokenStream {
+    let name = item.to_string();
+    let mut code = format!("impl {} ", name);
+    code += "{fn answer() -> u32 { 42 }}";
+
+    code.parse().unwrap()
+}
+
+#[proc_macro_derive(HelperAttr, attributes(helper))]
+pub fn derive_helper_attr(_item: TokenStream) -> TokenStream {
+    TokenStream::new()
+}
+
+#[proc_macro_attribute]
+pub fn return_as_is(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
+
+#[proc_macro_attribute]
+pub fn show_streams(attr: TokenStream, item: TokenStream) -> TokenStream {
+    println!("attr: \"{}\"", attr.to_string());
+    println!("item: \"{}\"", item.to_string());
+    item
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use proc_macrobrrrr::{make_answer, AnswerFn, HelperAttr, show_streams};
+
+#[macro_export]
+macro_rules! worse_vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
 }
+
+#[derive(HelperAttr)]
+struct Struct {
+    #[helper] lol: (),
+}
+
+#[derive(AnswerFn)]
+struct Hello;
+
+make_answer!();
+
+fn main() {
+    let hello = Hello;
+    let vec = worse_vec!["hello", "joe"];
+    println!("{:#?}", vec);
+    println!("{}", hello.answer());
+    println!("{}", answer());
+
+    // Example: Basic function
+    #[show_streams]
+    fn invoke1() {}
+    // out: attr: ""
+    // out: item: "fn invoke1() {}"
+
+    // Example: Attribute with input
+    #[show_streams(bar)]
+    fn invoke2() {}
+    // out: attr: "bar"
+    // out: item: "fn invoke2() {}"
+
+    // Example: Multiple tokens in the input
+    #[show_streams(multiple => tokens)]
+    fn invoke3() {}
+    // out: attr: "multiple => tokens"
+    // out: item: "fn invoke3() {}"
+
+    // Example:
+    #[show_streams { delimiters }]
+    fn invoke4() {}
+    // out: attr: "delimiters"
+    // out: item: "fn invoke4() {}"
+}
+
